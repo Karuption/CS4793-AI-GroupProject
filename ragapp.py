1aec86a9a96c1c9584c28a1d257dbb69631cc440
Karuption <pederson26@gmail.com>
2024-04-29 22:19:22 -0500

    Add a more complex UI. Now streams the answer like ChatGPT's responses

--- ragapp.py
+++ ragapp.py
@@ -1,20 +1,18 @@
-import nest_asyncio
-
-# Needed for jupyter notebooks to work with async tasks
-nest_asyncio.apply()
-
-# Llama-index has a really easy to use news article parser. It takes a list of urls as strings
 from llama_index.readers.web import NewsArticleReader
 from llama_index.core.schema import Document
 
 def get_article(link: str) -> Document:
     article_link = link
     article = NewsArticleReader(html_to_text=True).load_data([article_link])
-    return article[0]
+    if len(article) > 0:
+        return article[0]
+    else:
+        return None
 
 from langchain_chroma import Chroma
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 
+print("Setting up Embeddings")
 embeddings = OllamaEmbeddings(
     base_url="http://localhost:11434",
     model="nomic-embed-text",
@@ -23,17 +21,20 @@ embeddings = OllamaEmbeddings(
     num_thread=16,
 )
 
+print("Loading Vector Store")
 vectorstore = Chroma(
     persist_directory="./chroma", 
     embedding_function=embeddings
 )
 
+print("Getting Retriever")
 retriever = vectorstore.as_retriever(
     search_type="similarity",
 )
 
 from langchain_community.llms.ollama import Ollama
 
+print("setting up the LLM connection")
 llm = Ollama(
     base_url="http://localhost:11434",
     model="mistral",
@@ -59,6 +60,7 @@ Article: {question}
 Short Sentence:"""
 custom_rag_prompt = PromptTemplate.from_template(template)
 
+print("Making RAG chain")
 rag_chain = (
     {"context": retriever | format_retrieved_docs, "question": RunnablePassthrough()}
     | custom_rag_prompt
@@ -66,7 +68,8 @@ rag_chain = (
     | StrOutputParser()
 )
 
-from flask import Flask, send_file, request
+from flask import Flask, Response, send_file, request
+from flask_sse import sse
 
 app = Flask("RAG")
 
@@ -74,20 +77,40 @@ app = Flask("RAG")
 async def index():
     return send_file('index.html')    
 
+@app.route('/ragRequest', methods = ['POST'])
+async def tweet_generator():
+    article_link = request.form.get('articleTextbox')
+    return f"""
+    <div id="generatedText" class="px-1 pt-2 pb-0">
+        <div hx-ext="sse" sse-connect="/ragResponse/{article_link}" sse-swap="completed" hx-swap="outerHTML">
+            <div sse-swap="message" hx-swap="outerHTML">
+                <span class="spinner-border spinner-border-sm"></span>
+            </div>
+            </div>
+        </div>
+    </div>"""
 
 import time
-@app.route('/RAG', methods = ['POST'])
-async def tweet_generator():
-    time.sleep(1)
-    return """
-    <div class="form-group">
-        <textarea class="form-control input-bg" id="generatedText" rows="5" tabindex="-1"
-            readonly>{auhetasuhetansh}</textarea>
-    </div>"""
-    article_link = request.form.get('articleTextbox')
-    print("scraping article at " + article_link)
-    article = get_article(article_link)
-    print("Sending sending to rag chain")
-    return await rag_chain.ainvoke(article.metadata['summary'])
+@app.route("/ragResponse/<path:url>", methods=['GET'])
+def publish_hello(url=None):
+    def stream(url):
+        prefix = f"data: <p sse-swap=\"message\">"
+        msg = ""
+        end = "</p>\n\n"
+
+        if url == None:
+            yield "event: completed\ndata: <p style=\"color: red;\">Bad URL" + msg + end
+        
+        article = get_article(url)
+        if article == None:
+            yield "event: completed\ndata: <p style=\"color: red;\">Bad URL" + msg + end
+
+        for response in rag_chain.stream(article.metadata['summary']):
+            msg += response
+            yield prefix + msg + end
+            time.sleep(.15)
+        yield "event: completed\ndata: <p>" + msg + end
+
+    return Response(stream(url), mimetype="text/event-stream")
     
 app.run(debug=True)
